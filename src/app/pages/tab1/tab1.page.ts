b8b270e62f1c2bea4d6f15c30fc72fa5da52c9e8
jose <jlgarcia92@gmail.com>
2023-03-08 21:24:30 +0100

    Start of Lecture 109

--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {NewsService} from "../../services/news.service";
+import {Article} from "../../interfaces";
 
 @Component({
   selector: 'app-tab1',
@@ -8,15 +9,15 @@ import {NewsService} from "../../services/news.service";
 })
 export class Tab1Page implements OnInit {
 
+  public articles: Article[] = [];
+
   constructor(private newsService: NewsService) {
 
   }
 
   ngOnInit() {
     this.newsService.getTopHeadlines()
-      .subscribe(resp => {
-        console.log('noticias', resp);
-      })
+      .subscribe(resp => this.articles.push(...resp));
   }
 
 }
