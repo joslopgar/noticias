2893f65e854d446243e29635497c63a68221a986
jose <jlgarcia92@gmail.com>
2023-03-10 17:33:47 +0100

    Start of Lecture 120

--- src/app/components/article/article.component.ts
+++ src/app/components/article/article.component.ts
@@ -1,6 +1,7 @@
 import {Component, Input} from '@angular/core';
 import {Article} from "../../interfaces";
 import {Browser} from "@capacitor/browser";
+import {ActionSheetController} from "@ionic/angular";
 
 @Component({
   selector: 'app-article',
@@ -12,11 +13,48 @@ export class ArticleComponent {
   @Input() article: Article = {} as Article;
   @Input() index: number = 0;
 
+  constructor(private actionSheetController: ActionSheetController) {
+  }
+
   openArticle = async () => {
     await Browser.open({url: this.article.url});
   }
 
-  onClick() {
+  async onOpenMenu() {
+    const actionSheet = await this.actionSheetController.create({
+      header: 'Opciones',
+      buttons: [
+        {
+          text: 'Compartir',
+          icon: 'share-outline',
+          handler: () => {
+            this.onShareArticle();
+          }
+        }, {
+          text: 'Favorito',
+          icon: 'heart-outline',
+          handler: () => {
+            this.onToggleFavorite();
+          }
+        }, {
+          text: 'Cancel',
+          icon: 'close',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        }
+      ]
+    });
+
+    await actionSheet.present();
+  }
+
+  private onShareArticle() {
+
+  }
+
+  private onToggleFavorite() {
 
   }
 }
